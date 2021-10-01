d270b0a82123af3838a3f86d3b1a896ba3454619
Simon Xu <yhsimon.xu@gmail.com>
2021-09-30 23:43:24 -0600

    added pseudo commands that allow adding and removing channels from quotes and text

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
-import string
 import discord
 import random
 import os
 from discord.enums import ChannelType
+from discord.ext import commands
 from dotenv import load_dotenv
 
-load_dotenv()
 
-intents = discord.Intents.none()
-intents.reactions = True
-intents.members = True
-intents.guilds = True
+################ INITIALIZATION ################
+
+load_dotenv()
 
 TOKEN = os.getenv('TOKEN')
 GUILD_ID = int(os.getenv('GUILD_ID'))
 
 intents = discord.Intents.all()
-client = discord.Client(intents=intents)
+client = commands.Bot(intents=intents, command_prefix='!')
 
-members = []
-channels = []
-quotes_channels = []
+members = set()
+text_channels = set()
+quotes_channels = set()
 messages = []
 guild = None
+
+################################################
+
 
 def pick_random_quote(id):
     global messages
@@ -37,11 +38,38 @@ def pick_random_quote(id):
     return quote
 
 
+################ HELPER FUNCTIONS ################
+
+def get_messages(channel):
+    return channel.history(limit=200).flatten()
+
+def get_channel(channel_id):
+    return client.get_channel(int(channel_id))
+
+def add_quote_channel(channel_id):
+    quotes_channels.add(int(channel_id))
+
+def add_text_channel(channel_id):
+    text_channels.add(int(channel_id))
+
+#TODO clear all messages then retry to get messages
+def remove_quote_channel(channel_id):
+    quotes_channels.discard(int(channel_id))
+
+def remove_text_channel(channel_id):
+    text_channels.discard(int(channel_id))
+
+def get_channel_name(channel_id):
+    return get_channel(int(channel_id))
+
+################################################
+
+
 #TODO moves everything into a function
 @client.event
 async def on_ready():
     global messages
-    print("We have logged in as {}".format(client.user))
+    print("Quote bot is now ready to quote")
 
     for g in client.guilds:
         if g.id == GUILD_ID:
@@ -50,32 +78,80 @@ async def on_ready():
     
     if guild:
         for member in guild.members:
-            members.append(member)
+            members.add(member)
     else:
-        print("broken")
+        print("No guild found!")
 
-    #TODO change this to be passed as some sort of variable
-    channels = guild.text_channels
-    for channel in channels:
-        if channel.id == 892641837112770591:
-            quotes_channels.append(channel)
-
-    for channel in quotes_channels:
-        messages += await channel.history(limit=200).flatten()
-    
 
 @client.event
 async def on_message(message):
+    global messages
+    global text_channels
+    global quotes_channels
+    # Prevent infinite recursion causing our doom!
     if message.author == client.user:
         return
+
+    current_channel = message.channel
+
+#TODO change this to be actual commands
+################ COMMANDS ################
+
+    command = message.content
+
+    if command.startswith("!add_quote_channel "):
+        channel_ids = message.content.split(" ")
+        for channel_id in channel_ids[1:]:
+            # So this will fail if the user inputs \ since the string doesn't escape it properly
+            if channel_id.isdigit and get_channel(channel_id):
+                add_quote_channel(channel_id)
+                messages += await get_messages(get_channel(channel_id))
+        return
+
+    if command.startswith("!add_text_channel "):
+        channel_ids = message.content.split(" ")
+        for channel_id in channel_ids[1:]:
+            if channel_id.isdigit and get_channel(channel_id):
+                add_text_channel(channel_id)
+        return
+
+    if command.startswith("!remove_quote_channel "):
+        channel_ids = message.content.split(" ")
+        for channel_id in channel_ids[1:]:
+            if channel_id.isdigit and get_channel(channel_id):
+                remove_quote_channel(channel_id)
+        return
+
+    if command.startswith("!remove_text_channel "):
+        channel_ids = message.content.split(" ")
+        for channel_id in channel_ids[1:]:
+            if channel_id.isdigit() and get_channel(channel_id):
+                remove_text_channel(channel_id)
+        return
+
+    if command.startswith("!list_text_channels"):
+        await current_channel.send(
+            "**Text channels:** " + ','.join(map(str, [get_channel(text_channel) for text_channel in text_channels])) if text_channels else
+            "There are no text channels"
+        )
+        return
+
+    if command.startswith("!list_quote_channels"):
+        await current_channel.send(
+            "**Quotes channels:** " + ','.join(map(str, [get_channel(quotes_channel) for quotes_channel in quotes_channels])) if quotes_channels else
+            "There are no quotes channels" #ternary operator here
+        )
+        return
     
-    if message.channel.id == 892612985481203725:
+################################################
+
+    if message.channel.id in text_channels:
         for member in members:
             member_tag = "<@!{}>".format(member.id)
             if member_tag in message.content:
                 quote = pick_random_quote(member.id)
                 user_name = member.name
                 quote = "{}: {}".format(user_name, quote)
-                await message.channel.send(quote)
+                await current_channel.send(quote)
 
 client.run(TOKEN)
